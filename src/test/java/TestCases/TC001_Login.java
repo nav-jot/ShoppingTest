2f480ca85a34c1f884234ee4fa088d3741a21ee7
nav-jot <smilenavjot@gmail.com>
2019-01-07 10:39:47 -0500

    working on branches

--- src/test/java/TestCases/TC001_Login.java
+++ src/test/java/TestCases/TC001_Login.java
@@ -1,12 +1,18 @@
 package TestCases;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.qa.Base.TestBase;
 
@@ -14,24 +20,51 @@ public class TC001_Login extends TestBase {
 
 	@BeforeTest
 	public void setUp() {
-		System.setProperty(pathProperties.getProperty("chromeKey"), pathProperties.getProperty("chromeValue"));
+		System.setProperty(pathProperties.getProperty("chromeKey"), System.getProperty("user.dir")+pathProperties.getProperty("chromeValue"));
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(pathProperties.getProperty("url"));
 	}
 	
-	@Test
-	public void testLogin() {
+	@Test(priority = 1, dataProvider = "getData")
+	public void testLogin(String userName, String password) {
 		//driver.findElement(By.xpath(elementsProperties.getProperty("textUserID"))).sendKeys("mngr172213");
-		generic.enterText("textUserID", "mngr172213");
-		generic.enterText("textPassword", "zaqazev");
+		generic.enterText("textUserID", userName);
+		generic.enterText("textPassword", password);
 		generic.clickButton("buttonLogin");
-		System.out.print(driver.getTitle());
+		boolean alertPresent;
+		Alert alert = null;
+		SoftAssert sa = new SoftAssert();
+		try{
+			alert = driver.switchTo().alert();
+			alertPresent = true;
+		}
+		catch(NoAlertPresentException ex){
+			alertPresent = false;
+		}
+		if (alertPresent) {
+			alert.accept();
+			sa.assertEquals(alert.getText(), "User is not valid");
+		}
+		else
+		{
+		sa.assertEquals(driver.getTitle(),"GTPL Bank Manager HomePage");
+		}
 	}
 	
 	@AfterTest
 	public void cleanUp() {
-		//driver.close();
+		driver.close();
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] data = new Object[2][2];
+		data[0][0]="mngr172213";
+		data[0][1]="wrongpassword";
+		data[1][0]="mngr172213";
+		data[1][1]="zaqazev";
+		return data;
 	}
 }
